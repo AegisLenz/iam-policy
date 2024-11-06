272d7a05b83f031c3f6ed1e0eea8866f93090608
bolkv <bolkv0211@gmail.com>
2024-11-06 18:05:50 +0900

    Feat: Attack 고려 최소권한 정책 생성(#47)

--- src/extract_policy_by_cloudTrail.py
+++ src/extract_policy_by_cloudTrail.py
@@ -6,50 +6,64 @@ from s3_policy_mapper import s3_policy_mapper
 from ec2_policy_mapper import ec2_policy_mapper
 from iam_policy_mapper import iam_policy_mapper
 
+
+def making_policy(log_entry):
+    event_source = log_entry.get("eventSource")
+    event_name = log_entry.get("eventName")
+
+    if event_source == 's3.amazonaws.com':
+        specific_policy_path = os.path.join("./../AWSDatabase/S3", f'{event_name.casefold()}.json')
+        if os.path.exists(specific_policy_path):
+            policy_data = load_json(specific_policy_path)
+            policy = s3_policy_mapper(log_entry, policy_data)
+        else:
+            policy = map_etc(event_source, event_name)
+
+    elif event_source == 'ec2.amazonaws.com':
+        specific_policy_path = os.path.join("./../AWSDatabase/EC2", f'{event_name.casefold()}.json')
+        if os.path.exists(specific_policy_path):
+            policy_data = load_json(specific_policy_path)
+            policy = ec2_policy_mapper(log_entry, policy_data)
+        else:
+            policy = map_etc(event_source, event_name)
+                    
+    elif event_source == 'iam.amazonaws.com':
+        policy = iam_policy_mapper(log_entry)
+
+    else:
+        policy = map_etc(event_source, event_name)
+
+    return policy
+
 def extract_policy_by_cloudTrail(file_path):
     logs = load_json(file_path).get("Records",[])
     if not isinstance(logs, list):
         print("Error: The log file does not contain a valid list of log entries.")
         return
-
+    
+    normal_log = []
     all_policies = []
+
+    #attack에서만 단독적으로 사용된 권한 제외
     for log_entry in logs:
         if not isinstance(log_entry, dict):
             print("Error: Log entry is not a valid dictionary.")
             continue
 
-        event_source = log_entry.get("eventSource")
-        event_name = log_entry.get("eventName")
+        isAttack = log_entry.get("mitreAttackTactics")
+        policy = making_policy(log_entry)
+        if policy:
+            if isAttack is None:
+                normal_log.append(log_entry)
 
-        if event_source == 's3.amazonaws.com':
-            specific_policy_path = os.path.join("./../AWSDatabase/S3", f'{event_name.casefold()}.json')
-            if os.path.exists(specific_policy_path):
-                policy_data = load_json(specific_policy_path)
-                policy = s3_policy_mapper(log_entry, policy_data)
-            else:
-                policy = map_etc(event_source, event_name)
+    #Attack고려한 최소권한 추출
+    for log_entry in normal_log:
+        if not isinstance(log_entry, dict):
+            print("Error: Log entry is not a valid dictionary.")
+            continue
 
-            if policy:
-                    all_policies.append(policy)
-
-        elif event_source == 'ec2.amazonaws.com':
-            specific_policy_path = os.path.join("./../AWSDatabase/EC2", f'{event_name.casefold()}.json')
-            if os.path.exists(specific_policy_path):
-                policy_data = load_json(specific_policy_path)
-                policy = ec2_policy_mapper(log_entry, policy_data)
-            else:
-                policy = map_etc(event_source, event_name)
-
-            if policy:
-                    all_policies.append(policy)
-                    
-        elif event_source == 'iam.amazonaws.com':
-            policy = iam_policy_mapper(log_entry)
-            if policy:
-                all_policies.append(policy)
-        else:
-            policy = map_etc(event_source, event_name)
-            all_policies.append(policy)
+        policy = making_policy(log_entry)
+        all_policies.append(policy)
 
     if not all_policies:
         print("No valid policies were generated.")
